112a3b59ec1f15e120fe97f912f8ca6bfd316e79
Baduit <lena.bertho@protonmail.com>
2022-02-20 11:32:47 +0100

    Hide pokemon name in description

--- DailyPokemonHandler.py
+++ DailyPokemonHandler.py
@@ -9,6 +9,7 @@ class DailyPokemonHandler:
 
 	def update(self):
 		self.pokemon = get_random_pokemon('fr')
+		self.hide_pokemon_name_in_description()
 		now = datetime.datetime.now()
 		# Update time is 4 am, not the time when the app is launched
 		self.last_update = datetime.datetime(year=now.year, month=now.month, day=now.day, hour=4, minute=0, second=0, microsecond=0)
@@ -21,4 +22,7 @@ class DailyPokemonHandler:
 	def update_if_needed(self):
 		if self.is_update_needed():
 			self.update()
+
+	def hide_pokemon_name_in_description(self):
+		self.pokemon.description.replace(self.pokemon.name, '******')
 
