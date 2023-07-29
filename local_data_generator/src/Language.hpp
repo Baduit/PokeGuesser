33107b863a7f9d2d8fa153d0aa3bdbe70fb324fb
Lena Bertho <>
2023-07-29 22:52:46 +0200

    Use a modifier to automatically make my strong types serializable to json

--- local_data_generator/src/Language.hpp
+++ local_data_generator/src/Language.hpp
@@ -2,23 +2,15 @@
 
 #include <string>
 
-#include <nlohmann/json.hpp>
-
 #include <strong_type/strong_type.hpp>
 #include <strong_type/regular.hpp>
 #include <strong_type/ordered.hpp>
 
+#include <StrongTypeJsonModified.hpp>
+
 namespace pokemon
 {
 
-using Language = strong::type<std::string, struct Language_, strong::regular, strong::ordered>;
-
-void to_json(nlohmann::json& j, const Language& l) {
-	j = l.value_of();
-}
-
-void from_json(const nlohmann::json& j, Language& l) {
-	j.get_to(l.value_of());
-}
+using Language = strong::type<std::string, struct Language_, strong::regular, strong::ordered, SerializableToJson>;
 
 } // namespace pokemon
--- local_data_generator/src/Type.hpp
+++ local_data_generator/src/Type.hpp
@@ -2,23 +2,15 @@
 
 #include <string>
 
-#include <nlohmann/json.hpp>
-
 #include <strong_type/strong_type.hpp>
 #include <strong_type/regular.hpp>
 #include <strong_type/ordered.hpp>
 
+#include <StrongTypeJsonModified.hpp>
+
 namespace pokemon
 {
 
-using Type = strong::type<std::string, struct Type_, strong::regular, strong::ordered>;
-
-void to_json(nlohmann::json& j, const Type& t) {
-	j = t.value_of();
-}
-
-void from_json(const nlohmann::json& j, Type& t) {
-	j.get_to(t.value_of());
-}
+using Type = strong::type<std::string, struct Type_, strong::regular, strong::ordered, SerializableToJson>;
 
 } // namespace pokemon
