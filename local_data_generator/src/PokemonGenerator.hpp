6e6817b8d3e7997721ccca2723787421f693d95d
Lena Bertho <>
2023-07-29 22:15:46 +0200

    Fix include header

--- local_data_generator/src/PokemonGenerator.hpp
+++ local_data_generator/src/PokemonGenerator.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Pokemon.hpp>
+
 namespace pokemon
 {
 	
--- local_data_generator/src/main.cpp
+++ local_data_generator/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <PokemonGenerator.hpp>
+
 int main(int, char**)
 {
 	std::cout << "Hello, World!" << std::endl;
