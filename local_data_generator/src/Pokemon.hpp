69f9a60b0b824d6f0c7d8d36162d8936eae23f5b
Lena Bertho <>
2023-08-20 23:35:47 +0200

    Change the format of the pokemon

--- local_data_generator/src/Pokemon.hpp
+++ local_data_generator/src/Pokemon.hpp
@@ -8,24 +8,23 @@
 #include <Language.hpp>
 #include <Type.hpp>
 #include <units/lenght/Meter.hpp>
-#include <units/lenght/Decimeter.hpp>
 #include <units/mass/Kilogram.hpp>
-#include <units/mass/Hectogram.hpp>
 
 namespace pokemon
 {
 
 struct Pokemon
 {
+	std::size_t id;
 	std::map<Language, std::string> names;
-	std::map<Language, std::string> descriptions;
+	std::map<Language, std::vector<std::string>> descriptions;
 	Meter height;
 	Kilogram weight;
-	std::size_t generation;
-	std::map<Language, Type> types;
+	std::map<Language, std::vector<Type>> types;
+	std::string image_url;
 };
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Pokemon, names, descriptions, height, weight, generation, types)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Pokemon, id, names, descriptions, height, weight, types, image_url)
 
 } // namespace pokemon
 
